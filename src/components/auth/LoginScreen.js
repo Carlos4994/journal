c22b79b14140a5071e3bdc91f11f9508a68eaca8
Carlos Morales <carlosluismorales1@gmail.com>
2023-10-08 16:26:50 -0500

    first dispatch

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,46 +1,75 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useForm } from '../../hooks/useForm';
+import { login } from '../../actions/auth';
 
 
 
 export const LoginScreen = () => {
+
+
+  const dispatch= useDispatch();
+  
+  const [formValue, handleInputChange] = useForm({
+    email: 'carlosluismorles1@gmail.com',
+    password: '12345'
+  })
+
+  // desestructurar
+
+  const { email, password } = formValue;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(login('12345','Chuck'))
+
+  }
   return (
     <div>
 
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
-          <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"> Login Screen </h2>
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img className="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"> Login Screen </h2>
         </div>
 
-        
 
-        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form class="space-y-6">
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleLogin}>
             <div>
-              <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-              <div class="mt-2">
-                <input name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <label className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+              <div className="mt-2">
+                <input name="email" type="email" required 
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  value={email}
+                  onChange={handleInputChange}
+                />
               </div>
             </div>
 
             <div>
-              <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-              <div class="mt-2">
-                <input name="Password" type="password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <label className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+              <div className="mt-2">
+                <input name="password" type="password" required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  value={password}
+                  onChange={handleInputChange}
+                />
               </div>
             </div>
 
             <div>
-              <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Login</button>
+              <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Login</button>
             </div>
 
           </form>
 
-          <p class="mt-10 text-center text-sm text-gray-500">
+          <p className="mt-10 text-center text-sm text-gray-500">
             Not a member? <Link
-            to='/auth/register'
-            class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+              to='/auth/register'
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >Register</Link>
           </p>
         </div>
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,3 @@
-import { createStoreHook } from "react-redux";
 import { combineReducers, createStore } from "redux";
 import { authReducer } from "../reducers/authReducer";
 
@@ -8,4 +7,6 @@ const reducers = combineReducers({
 })
 
 
-export const store = createStore(reducers)
+export const store = createStore(reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
